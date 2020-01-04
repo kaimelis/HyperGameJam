1fb4c3de8ac561bad6070de194d2ac7a5eccd4e6
Kristupas Antanavicius <antanaviciuskristupas@gmail.com>
2020-01-04 15:49:48 +0200

    Set slices textures

---
+++ HyperGameJam/Assets/LavaAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaAnimation : MonoBehaviour
+{
+    public float maxOffset = 1.0f;
+    public float offsetIncrement = 0.1f;
+    public float frameTime = 0.3f;
+    private float timer;
+    private float offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = Random.Range(0, 1);
+        timer = Random.Range(0, frameTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= frameTime)
+        {
+            timer = 0;
+            offset = (offset + offsetIncrement) % maxOffset;
+            Debug.Log("AAA " + offset);
+            GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offset, 0.0f);
+        }
+    }
+}
