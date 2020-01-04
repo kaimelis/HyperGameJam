290766330924e195940514d188650e721ecc9d9e
Haru <53146589+H-a-r-u@users.noreply.github.com>
2020-01-04 16:19:01 +0200

    ui
    zodziu bandom

---
+++ HyperGameJam/Assets/Scripts/UI/Menu/MenuSettingsGear.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSettingsGear : MonoBehaviour
+{
+    [SerializeField]
+    GameObject soundsButton, creditsButton;
+    [SerializeField]
+    SettingsScriptable settingsScriptable;
+    public void ToggleSettings()
+    {
+        if (settingsScriptable.activeSettings)//Hide
+        {
+            settingsScriptable.activeSettings = false;
+            soundsButton.SetActive(false);
+            creditsButton.SetActive(false);
+        }
+        else//Show
+        {
+            settingsScriptable.activeSettings = true;
+            soundsButton.SetActive(true);
+            creditsButton.SetActive(true);
+        }
+
+    }
+}
