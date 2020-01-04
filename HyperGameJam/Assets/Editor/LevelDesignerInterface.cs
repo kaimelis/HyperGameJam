880f2cbd7ccfc6281a126b8753bfa8e63668b5b2
Kasparas <kasparas.kazemekas@gmail.com>
2020-01-04 12:43:11 +0200

    Level designer ir gitignore

---
+++ HyperGameJam/Assets/Editor/LevelDesignerInterface.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(LevelDesigner), true)]
+public class LevelDesignerInterface : Editor
+{
+    LevelDesigner levelDesigner;
+
+    //  bool showPosition;
+    int temp;
+
+    private void OnEnable()
+    {
+        levelDesigner = (LevelDesigner) target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+
+        levelDesigner = (LevelDesigner) target;
+
+        //shows public variables from base class
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Generate", GUILayout.Width(190)))
+        {
+          levelDesigner.Generate();
+
+        }
+    }
+}
---
+++ HyperGameJam/Assets/LevelDesigner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public class LevelDesigner : MonoBehaviour
+{
+    public int pieCount;
+    public GameObject pref;
+
+    private const int pieSlicesPerPie = 8;
+    const float degreePerSlice = 360 / pieSlicesPerPie;
+
+    private const float gap = -5f;
+
+    private List<List<GameObject>> level;
+
+    private Transform parent;
+    public void Generate()
+    {
+        if (parent != null)
+            DestroyImmediate(parent.gameObject);
+
+        parent = new GameObject().transform;
+
+        level = new List<List<GameObject>>();
+        for (int i = 0; i < pieCount; i++)
+        {
+            CreatePie(i);
+        }
+    }
+
+    void CreatePie(int pieId)
+    {
+        level.Add(new List<GameObject>());
+        for (int i = 0; i < pieSlicesPerPie; i++)
+        {
+            GameObject slice = Instantiate(pref,new Vector3(0,pieId * gap,0), Quaternion.Euler(new Vector3(0, i * degreePerSlice, 0)));
+            slice.transform.parent = parent;
+            level[pieId].Add(slice);
+        }
+    }
+
+    void CreateMidPole()
+    {
+
+    }
+}
